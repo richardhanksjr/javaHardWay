d7d236291334eca7a72b03825a79b7166db18b88
phattyroses <phattyroses@gmail.com>
2016-02-05 21:47:13 -0500

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+ public class FirstProg{
+ 	public static void main( String[] args){
+ 		System.out.println("I, Richard Hanks, am determined to learn how to code");
+ 		System.out.println( "Today's date is Friday, February 5, 2016.");
+ 	}
+ }
